eaa4537906a02f87deb4344c3430f8aa7d67c88d
Robbert Batterink <robbertbatterink@hotmail.nl>
2019-07-16 00:44:11 +0200

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.4a534bed7b7ca6ab1d63018bcf8d5197.js"
 );
 
 self.addEventListener('message', (event) => {
