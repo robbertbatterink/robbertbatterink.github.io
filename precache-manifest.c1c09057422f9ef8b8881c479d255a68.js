f728e5d6c2bb2eaf878dd1db77ba37c4044be905
Robbert Batterink <robbertbatterink@hotmail.nl>
2019-07-16 13:31:09 +0200

    Updates

---
+++ precache-manifest.c1c09057422f9ef8b8881c479d255a68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90181e3412bc9b9f12a31a6edfdf3fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3333dcaf27be21ccbbad",
+    "url": "/static/css/main.5031556d.chunk.css"
+  },
+  {
+    "revision": "64a9263736765b36d26a",
+    "url": "/static/js/2.c50df4d7.chunk.js"
+  },
+  {
+    "revision": "3333dcaf27be21ccbbad",
+    "url": "/static/js/main.10db535a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "15861e3bb65a4bd6ea7b40f6f6f1795c",
+    "url": "/static/media/html-programming.15861e3b.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b84f39fe5c34a996c655bf2d407feef1",
+    "url": "/static/media/picture.b84f39fe.jpg"
+  }
+]);
