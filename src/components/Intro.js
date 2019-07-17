8420778c75e85bcd096ecc23247ccada0e1d8b25
Robbert Batterink <robbertbatterink@hotmail.nl>
2019-07-17 23:13:01 +0200

    carousel

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,8 @@ import './containers.css';
 
 import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import { Carousel, CarouselSlide } from 'material-ui-carousel'
-import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import westerbork from "./Westerbork.JPG";
 import lariks from "./lariks.jpg";
@@ -12,39 +13,78 @@ import entrance from "./entrance.jpg";
 import quinto from "./quinto.png"
 
 class Intro extends Component {
-    pictures = [
-      {image: westerbork, title: 'Westerbork'},
-      {image: lariks, title: 'Vincent van Gogh'},
-      {image: hanze, title: 'Hanze'},
-      {image: entrance, title: 'Entrance'},
-      {image: quinto, title: 'Quinto'},
-        ];
+    constructor(){
+        super();
+        this.state = {
+            pictures: [
+              {image: westerbork, title: 'Westerbork', index: 0},
+              {image: lariks, title: 'Vincent van Gogh', index: 1},
+              {image: hanze, title: 'Hanze', index: 2},
+              {image: entrance, title: 'Entrance', index: 3},
+              {image: quinto, title: 'Quinto', index: 4},
+          ],
+            }
+    }
     render() {
+        // <div className="slideshow">
+        //     <Carousel data-ride="carousel" >
+        //        {this.state.pictures.map(({ image, title }) => (
+        //          <CarouselSlide key={image}>
+        //            <Card>
+        //              <CardMedia
+        //                image={image}
+        //                title={title}
+        //                style={{
+        //                  height: '100%',
+        //                  paddingTop: '50%',
+        //                }}
+        //              />
+        //              <CardContent>
+        //                <Typography>{title}</Typography>
+        //              </CardContent>
+        //            </Card>
+        //          </CarouselSlide>
+        //        ))}
+        //      </Carousel>
+        // </div>
         return(
             <div style={{backgroundColor: "#6AA4B8"}}>
                 <div className="containerLeft bg-dark">
                         <h1>About me</h1>
                         <div className="slideshow">
-                            <Carousel data-ride="carousel" >
-                               {this.pictures.map(({ image, title }) => (
-                                 <CarouselSlide key={image}>
-                                   <Card>
-                                     <CardMedia
-                                       image={image}
-                                       title={title}
-                                       style={{
-                                         height: '100%',
-                                         paddingTop: '50%',
-                                       }}
-                                     />
-                                     <CardContent>
-                                       <Typography>{title}</Typography>
-                                     </CardContent>
-                                   </Card>
-                                 </CarouselSlide>
-                               ))}
-                             </Carousel>
-                        </div>
+                        <CarouselProvider
+                              style={{height: "50%"}}
+                              naturalSlideWidth={100}
+                              naturalSlideHeight={60}
+                              totalSlides={this.state.pictures.length}
+                              isPlaying={true}
+                              dragEnabled={false}
+                            >
+                              <Slider>
+                                  {this.state.pictures.map(({ image, title, index }) => (
+                                    <Slide index={index} key={image}>
+                                      <Card>
+                                        <CardMedia
+                                          image={image}
+                                          title={title}
+                                          style={{
+                                            height: '100%',
+                                            paddingTop: '50%',
+                                          }}
+                                        />
+                                        <CardContent>
+                                          <Typography>{title}</Typography>
+                                        </CardContent>
+                                      </Card>
+                                    </Slide>
+                                  ))}
+                              </Slider>
+                              <div className="text-center" style={{paddingTop:5}}>
+                              <ButtonBack type="button" className="btn btn-info">Back</ButtonBack>
+                              <ButtonNext type='button' className="btn btn-info" style={{marginLeft:15}}>Next</ButtonNext>
+                              </div>
+                            </CarouselProvider>
+                            </div>
                         <div className="intro" id="introduction">
                             My name is Robbert Batterink and I live in the village of
                             Westerbork in Drenthe, The Netherlands. I am a student
